457caa34b761af012141f6397cee1dbf40d56a33
xenonflash <jzykc@hotmail.com>
2018-05-02 17:37:19 +0800

    Create gtm-events.js
    gtm events  declaration

---
+++ gtm-events.js
@@ -0,0 +1,9 @@
+var customizeGtmEvents = [
+  ['SELECTOR', 'click', {
+    'event': 'CustomEvent',
+    'event_category':'XXXX',
+    'event_action': 'XXXX',
+    'event_label': 'XXXX'
+  }],
+  ...
+]
